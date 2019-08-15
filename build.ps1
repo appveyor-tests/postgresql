402f367db955f9e4863b60dd9db23084683f3bb7
Alena Hrynkevich <alena@appveyor.com>
2019-08-15 16:30:43 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,10 +1,11 @@
 $env:isVs2017 = 'false'
 if (test-path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community") { $env:isVs2017 = 'true' }
 if ($env:isVs2017 -eq 'true' -and ($env:PGSQL_VERSION -eq '9.3' -or $env:PGSQL_VERSION -eq '9.4' -or $env:PGSQL_VERSION -eq '9.5')) { Exit-AppveyorBuild }
+Write-Host "PGSQL_VERSION:"
 $env:PGSQL_VERSION
 Start-Service "postgresql-x64-$env:PGSQL_VERSION"
-$env:PGUSER=postgres
-$env:PGPASSWORD=Password12!
+$env:PGUSER = 'postgres'
+$env:PGPASSWORD = Password12!
 $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
 $newpath = "$oldpath;C:\Program Files\PostgreSQL\$env:PGSQL_VERSION\bin\"
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
