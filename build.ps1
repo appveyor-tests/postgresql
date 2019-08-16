0179636ffd5acf6e2364fb2825395c452bf7b613
Alena Hrynkevich <alena@appveyor.com>
2019-08-16 16:23:35 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $env:isVs2017 = 'false'
 if (test-path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community") { $env:isVs2017 = 'true' }
 if ($env:isVs2017 -eq 'true' -and ($env:PGSQL_VERSION -eq '9.3' -or $env:PGSQL_VERSION -eq '9.4' -or $env:PGSQL_VERSION -eq '9.5')) { Exit-AppveyorBuild }
-$s_name = postgresql-x64-$env:PGSQL_VERSION
+$s_name = "postgresql-x64-$env:PGSQL_VERSION"
 $s_name
 Start-Service "postgresql-x64-$env:PGSQL_VERSION"
 Start-Sleep -s 10
