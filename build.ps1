9ef0057ae0387ba795b4bf1488ead94db3b093a7
Alena Hrynkevich <alena@appveyor.com>
2019-08-17 20:18:22 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,7 +1,10 @@
 $env:isVs2017 = 'false'
 if (test-path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community") { $env:isVs2017 = 'true' }
 Write-Host "Is VS 2017?: $env:isVs2017"
-if ($env:isVs2017 -eq 'true' -and ($env:PGSQL_VERSION -eq '9.3' -or $env:PGSQL_VERSION -eq '9.4' -or $env:PGSQL_VERSION -eq '9.5')) { Write-Host "It's Visual Sturdio 2017"; Exit-AppveyorBuild }
+if ($env:isVs2017 -eq 'true' -and ($env:PGSQL_VERSION -eq '9.3' -or $env:PGSQL_VERSION -eq '9.4' -or $env:PGSQL_VERSION -eq '9.5')) { 
+  Write-Host "It's Visual Sturdio 2017, exiting..."; 
+  cmd /c Exit-AppveyorBuild 
+}
 $s_name = "postgresql-x64-$env:PGSQL_VERSION"
 dir "C:\Program Files\PostgreSQL\"
 Start-Service "postgresql-x64-$env:PGSQL_VERSION"
