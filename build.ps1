f51ed729f3fd0137b64407168a7d1a236642576c
Alena Hrynkevich <alena@appveyor.com>
2019-08-17 19:57:16 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,7 +2,6 @@ $env:isVs2017 = 'false'
 if (test-path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community") { $env:isVs2017 = 'true' }
 if ($env:isVs2017 -eq 'true' -and ($env:PGSQL_VERSION -eq '9.3' -or $env:PGSQL_VERSION -eq '9.4' -or $env:PGSQL_VERSION -eq '9.5')) { Exit-AppveyorBuild }
 $s_name = "postgresql-x64-$env:PGSQL_VERSION"
-$s_name
 dir "C:\Program Files\PostgreSQL\"
 dir "C:\Program Files\PostgreSQL\$env:PGSQL_VERSION\bin\"
 Start-Service "postgresql-x64-$env:PGSQL_VERSION"
@@ -16,5 +15,6 @@ $env:PGPASSWORD = 'Password12!'
 
 
 cmd /c 'C:\Program Files\PostgreSQL\9.3\bin\createdb.exe' TestDatabase
+if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 
 
