2106db9af8d7e9593be1800bf0b3048a52939090
Feodor Fitsner <feodor@appveyor.com>
2020-02-27 11:07:00 -0800

    Added messages

--- test_pgsql.ps1
+++ test_pgsql.ps1
@@ -2,15 +2,22 @@ $pg_versions = @('9.3', '9.4', '9.5', '9.6', '10')
 if (test-path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community") { $pg_versions = @('9.6', '10') }
 if (test-path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community") { $pg_versions = @('9.6', '10', '11', '12') }
 
-foreach($pg_version in $pg_versions) {
+dir "C:\Program Files\PostgreSQL\"
+$env:PGUSER = 'postgres'
+$env:PGPASSWORD = 'Password12!'
+
+foreach ($pg_version in $pg_versions) {
 	$svc_name = "postgresql-x64-$pg_version"
-	dir "C:\Program Files\PostgreSQL\"
+	Write-Host "Starting $svc_name..."
 	Start-Service $svc_name
+	
 	get-service $svc_name | select Displayname,Status,ServiceName
-	$env:PGUSER = 'postgres'
-	$env:PGPASSWORD = 'Password12!'
 	$env:path = "C:\Program Files\PostgreSQL\$pg_version\bin;$env:path"
+	
+	Write-Host "Creating test database..."
 	cmd /c createdb.exe TestDatabase
 	if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+	
+	Write-Host "Stopping $svc_name..."
 	Stop-Service $svc_name
 }
